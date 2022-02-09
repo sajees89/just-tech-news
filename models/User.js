c9f5d709b4d201fe02be72b3c17cef867d715d17
Sajees XPS <sajees.jeya@gmail.com>
2022-02-09 02:56:52 -0500

    updated pass

--- models/User.js
+++ models/User.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const { Model, DataTypes } = require('sequelize'); 
 const sequelize = require('../config/connection'); 
 
@@ -45,6 +46,19 @@ User.init(
         }
       },
       {
+        hooks: {
+            // set up beforeCreate lifecycle "hook" functionality 
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10); 
+                return newUserData;
+                },
+                // set up beforeUpdate lifecycle "hook" functionality
+                async beforeUpdate(updatedUserData) {
+                    updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10); 
+                    return updatedUserData;
+                }
+            },
+        
         sequelize,
         timestamps: false,
         freezeTableName: true,
@@ -52,5 +66,45 @@ User.init(
         modelName: 'user'
       }
 );
+
+// User.init(
+//     {
+//       id: {
+//         type: DataTypes.INTEGER,
+//         allowNull: false,
+//         primaryKey: true,
+//         autoIncrement: true
+//       },
+//       username: {
+//         type: DataTypes.STRING,
+//         allowNull: false
+//       },
+//       email: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//         unique: true,
+//         validate: {
+//           isEmail: true
+//         }
+//       },
+//       password: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//         validate: {
+//           len: [4]
+//         }
+//       }
+//     },
+//     {
+//       hooks: {
+  
+//       },
+//       sequelize,
+//       timestamps: false,
+//       freezeTableName: true,
+//       underscored: true,
+//       modelName: 'user'
+//     }
+//   );
 
 module.exports = User; 
