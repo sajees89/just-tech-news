2451a8ed1e5d58bd5e26aaf956c8e8c541aa1030
Sajees XPS <sajees.jeya@gmail.com>
2022-03-08 00:13:14 -0500

    partials

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -85,12 +85,14 @@ router.post('/', (req, res) => {
 
 router.put('/upvote', (req, res) => {
   // custom static method created in models/Post.js
-  Post.upvote(req.body, { Vote })
+  if (req.session) {
+  Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
     .then(updatedPostData => res.json(updatedPostData))
     .catch(err => {
       console.log(err);
-      res.status(400).json(err);
+      res.status(500).json(err);
     });
+  }
 });
 
 router.put('/:id', (req, res) => {
